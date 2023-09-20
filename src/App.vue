415997dff3dbc9c5f9c967015619c58570a40d46
efrain <efrain.akl@gmail.com>
2023-09-20 13:15:49 -0400

    full

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import axios from 'axios'
+
+  const bcvPrice = ref(0);
+
+   const obtenerBcv = onMounted( async ()=>{
+   const { data } = await axios.get('https://api-bcv.vercel.app/bcv');
+   console.log(data);
+   bcvPrice.value = data.usd;
+
+  })
+
+</script>
+<template>
+  <div>
+    <h1>Precio del BCV</h1>
+    <p>{{ bcvPrice }}</p>
+  </div>
+</template>
+  
