907b9725bf1cc50b523e9b8085de2059dc00cc1c
efrain <efrain.akl@gmail.com>
2023-09-21 15:05:28 -0400

    ULTIMOS CAMBIOS

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <script setup>
   import { ref, onMounted } from 'vue'
   import axios from 'axios'
+  const url = 'https://bcv-api.vercel.app/bcv';
+  const urlMenu = 'http://localhost:3000/menu';
 
   const bcvPrice = ref(0);
+  const menu = ref(0);
 
    const obtenerBcv = onMounted( async ()=>{
-   const { data } = await axios.get('https://bcv-api.vercel.app/bcv');
+   const { data } = await axios.get(url);
    console.log(data);
    bcvPrice.value = data.usd;
-
   })
 
+   const getMenu = onMounted( async ()=>{
+    const { data } = await axios.get(urlMenu);
+    menu.value = data;
+    console.log(data)
+
+   })
 </script>
+
 <template>
   <div>
     <h1>Precio del BCV</h1>
     <p>{{ bcvPrice }}</p>
+    <div v-for="item in menu">
+      <p>{{item.nombre}} {{item.desc}} Precio: {{item.precio}}$</p>
+    </div>
   </div>
 </template>
   
